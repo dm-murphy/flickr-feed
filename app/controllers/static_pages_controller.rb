0541adf8351cdce285a8d499a54b97a301c4d1df
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-04-27 21:28:10 -0400

    Update #index in StaticPagesController to retrieve Flickr photostream

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,17 @@
 class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new
+
+    if params[:flickr_id]
+      @flickr_id = flickr_params
+
+      @photos = flickr.people.getPublicPhotos :user_id => @flickr_id
+    end
+  end
+
+  private
+
+  def flickr_params
+    params[:flickr_id]
   end
 end
